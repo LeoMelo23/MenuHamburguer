4b785dac4421178a51990cbbad5f4d1064ea6d97
LeoMelo23 <leomelo2332@gmail.com>
2026-04-02 10:58:23 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector("#menu");
+let btnMenu = document.querySelector(".material-symbols-outlined");
+
+btnMenu.addEventListener("click",function(){
+    menu.classList.toggle("ativo");
+})
